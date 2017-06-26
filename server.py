223bc4294756ee0f8dac93d7d918ddcd234a642f
Johannes Morgenroth <jm@m-network.de>
2017-06-26 09:15:56 +0200

    Working audio forward

--- server.py
+++ server.py
@@ -2,11 +2,30 @@
 
 import time
 import pymumble.pymumble_py3 as pymumble
+import pyaudio
+import numpy as np
+
+# global mumble object
+mumble = None
+
+# state for voice activation
+state = {
+    'level': 0.0,
+    'count': 0,
+    'maxcount': 3,
+    'activation_level': np.iinfo(np.uint16).max * 0.6
+}
+
+# radio state and buffer
+radio_buffer = b''
+radio_state = 0
 
 
 def bot(host, nickname):
+    global mumble
+
     # create the mumble instance
-    mumble = pymumble.Mumble(host, user=nickname, port=64738)
+    mumble = pymumble.Mumble(host, user=nickname, port=64738, debug=False)
 
     # identify this software
     mumble.set_application_string("dmr-bridge")
@@ -26,7 +45,44 @@ def bot(host, nickname):
     mumble.is_ready()
     mumble.users.myself.unmute()
 
-    time.sleep(30.0)
+    # open audio interface
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(2),
+                    channels=1,
+                    rate=48000,
+                    input=True,
+                    output=True,
+                    stream_callback=callback_audio,
+                    frames_per_buffer=4096)
+
+    stream.start_stream()
+
+    while stream.is_active():
+        time.sleep(0.1)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+
+def voice_detection(in_data):
+    global state
+
+    # look for high input level
+    input_frames = np.fromstring(in_data, dtype=np.uint16)
+
+    # store current average level
+    state['level'] = np.mean(input_frames)
+
+    if state['level'] < (state['activation_level'] * 0.8) and state['count'] > 0:
+        state['count'] = state['count'] - 1
+    elif state['level'] > (state['activation_level'] * 1.2) and state['count'] < state['maxcount']:
+        if state['count'] < 2:
+            state['count'] = 2
+        state['count'] = state['count'] + 1
+
+    return state['count'] > 0
 
 
 def callback_connected():
@@ -38,7 +94,44 @@ def callback_received_message(data):
 
 
 def callback_received_sound(user, chunk):
-    pass
+    global radio_buffer, radio_state
+
+    # copy input to mumble input buffer
+    radio_buffer = radio_buffer + chunk.pcm
+
+
+def get_radio_output(frame_count):
+    global radio_buffer, radio_state
+    chunk_size = frame_count * 2
+
+    if radio_state == 0 and len(radio_buffer) < chunk_size:
+        # fill front with silence
+        ret = (b'\0' * (chunk_size - len(radio_buffer))) + radio_buffer
+        radio_state = 1
+        radio_buffer = b''
+    elif radio_state == 1 and len(radio_buffer) < chunk_size:
+        # fill back with silence
+        ret = radio_buffer + (b'\0' * (chunk_size - len(radio_buffer)))
+        radio_state = 0
+        radio_buffer = b''
+    elif len(radio_buffer) > chunk_size:
+        # chop some frames from the buffer
+        ret = radio_buffer
+        radio_buffer = radio_buffer[chunk_size:]
+    else:
+        # generate silence
+        ret = np.zeros(frame_count, dtype=np.int16)
+
+    return ret
+
+
+def callback_audio(in_data, frame_count, time_info, status):
+    if voice_detection(in_data):
+        # copy radio input to mumble output buffer
+        mumble.sound_output.add_sound(in_data)
+
+    # copy buffered mumble input buffer to radio output
+    return get_radio_output(frame_count), pyaudio.paContinue
 
 
 if __name__ == "__main__":
